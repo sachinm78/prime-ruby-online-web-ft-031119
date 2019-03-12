e262020f60f8c86b8b1b82664fc6280a5f1b3912
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 21:16:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,6 @@
 # Add  code here!
 
-def prime? (n)
-    if n <= 1
-        false
-    elsif n == 2
-        true
-    else 
-        (2..n/2).any? { |i| n % i != 0}
-    end
+def prime?(n)
+      return false if n < 2
+      (2..Math.sqrt(n)).none? {|num| length % num == 0}
 end
